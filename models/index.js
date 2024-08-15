2a150ee62975980c777d2be5973bc5119d98c05f
demichele-c <demichele.charles@yahoo.com>
2024-08-15 11:10:40 -0400

    addded comment, post, user and index models

---
+++ models/index.js
@@ -0,0 +1,45 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// User has many Posts
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+// Post belongs to User
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+// Comment belongs to User
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+// Comment belongs to Post
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+});
+
+// User has many Comments
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+// Post has many Comments
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+});
+
+module.exports = {
+  User,
+  Post,
+  Comment,
+};
