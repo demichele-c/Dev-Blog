fc5379071645e05e3de5412d45970d8f56e056a1
demichele-c <demichele.charles@yahoo.com>
2024-09-03 20:34:35 -0400

    fixed comment section

--- public/js/comment.js
+++ public/js/comment.js
@@ -62,7 +62,47 @@ document.addEventListener('DOMContentLoaded', () => {
     } catch (error) {
       console.error('Error:', error);
       alert('An error occurred. Please try again.');
+    }//function to comment on a post
+async function commentFormHandler(event) {
+  event.preventDefault();
+
+  //get comment text from form
+  const comment_text = document
+    .querySelector('textarea[name="comment-body"]')
+    .value.trim();
+
+  // get id from url
+  const post_id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  //if statement (cannot post empty comment)
+  if (comment_text) {
+    //use comments POST API
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({
+        post_id,
+        comment_text,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    //if successful reload page with new comment
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
     }
+  }
+}
+
+//event listener for add comment button
+document
+  .querySelector(".comment-form")
+  .addEventListener("submit", commentFormHandler);
+
   }
 
   // Attach event listeners
